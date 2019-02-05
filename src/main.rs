2e4edc3e64dd411eefdbc20e0757d7ba4eca1027
Lonnie L. Souder <lonnie@souderfamily.net>
2019-02-04 22:45:44 -0500

    Set the pin to be an output.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 extern crate sysfs_gpio;
 
-use sysfs_gpio::{Pin};
+use sysfs_gpio::{Direction, Pin};
 use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
-    println!("Hello, world!");
-    let my_led = Pin::new(127); // number depends on chip, etc.
+    println!("Blinky, Blinky!");
+    let my_led = Pin::new(8); // number depends on chip, etc.
     my_led.with_exported(|| {
+        my_led.set_direction(Direction::Low)?;
         loop {
-            my_led.set_value(0).unwrap();
+            my_led.set_value(0).expect("nope, no zero for you!");
+
             sleep(Duration::from_millis(200));
-            my_led.set_value(1).unwrap();
+            my_led.set_value(1).expect("and no again!");
             sleep(Duration::from_millis(200));
         }
-    }).unwrap();
+    }).expect("can't export my led.");
 }
