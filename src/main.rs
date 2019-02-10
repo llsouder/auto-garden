e795fc97ff60c94e034c59b5135d270d12deb894
Lonnie Souder <lonnie@souderfamily.net>
2019-02-10 01:45:07 +0000

    Read light sensor on 21.  If lights are off flash LED on pin 20.

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,30 @@ use std::io::{self, Write};
 
 fn main() {
     println!("Blinky, Blinky!");
-    let my_led = Pin::new(8); // number depends on chip, etc.
-    my_led.with_exported(|| {
-        my_led.set_direction(Direction::Low)?;
-        loop {
-            my_led.set_value(0).expect("nope, no zero for you!");
-            print!("\rOn ");
-            io::stdout().flush().unwrap();
-            sleep(Duration::from_millis(1000));
-            my_led.set_value(1).expect("and no again!");
-            print!("\rOff");
-            io::stdout().flush().unwrap();
-            sleep(Duration::from_millis(1000));
-        }
-    }).expect("can't export my led.");
+    let my_led = Pin::new(20); // number depends on chip, etc.
+    let my_sensor = Pin::new(21);
+    my_led.export().unwrap();
+    my_sensor.export().unwrap();
+	my_led.set_direction(Direction::Low).unwrap();
+	my_sensor.set_direction(Direction::In).unwrap();
+	loop {
+		let sense = my_sensor.get_value().unwrap();
+		if sense == 0
+		{
+			my_led.set_value(1).expect("nope, no zero for you!");
+			print!("\rLights Off ");
+			io::stdout().flush().unwrap();
+		}
+		else 
+		{
+			my_led.set_value(0).expect("nope, no zero for you!");
+			print!("\rLights On ");
+			io::stdout().flush().unwrap();
+		}
+		
+		sleep(Duration::from_millis(250));
+		my_led.set_value(0).expect("and no again!");
+		io::stdout().flush().unwrap();
+		sleep(Duration::from_millis(250));
+	}
 }
