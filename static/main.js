7fb7baaa1eefce8172b6dced7aff3d8255506219
Lonnie L. Souder <lonnie@souderfamily.net>
2019-02-12 09:42:01 -0500

    Add toggle LED button to the webpage.

--- static/main.js
+++ static/main.js
@@ -1,5 +1,12 @@
-function update_light_indicator(signal_text) {
+function toggleLed() {
+    const ledHttp = new XMLHttpRequest();
+    const ledurl="http://" + location.host + "/toggle_led";
+    ledHttp.open("GET", ledurl);
+    ledHttp.send();
+}
 
+function update_light_indicator(signal_text) {
+    console.log("update_light_indicator:" + signal_text);
     if(document.getElementById("myDIV")!=null) {
         document.getElementById("myDIV").innerHTML = signal_text + "<br>";
         if(signal_text.includes("On")) {
@@ -9,15 +16,17 @@ function update_light_indicator(signal_text) {
         }
     }
  }
-
+    //server will send light status every 3s
     var ws = new WebSocket("ws://" + location.hostname + ":3012");
     ws.onmessage = function (evt) {
      update_light_indicator(evt.data);
     }
     const Http = new XMLHttpRequest();
     Http.onreadystatechange=(e)=>{
+    console.log("onreadystatechange" + e);
       update_light_indicator(Http.responseText);
     }
+    //return a one time light status
     const url="http://" + location.host + "/get_status";
     Http.open("GET", url);
     Http.send();
