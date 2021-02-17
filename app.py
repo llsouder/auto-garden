029b29745e05f5c91a38ee85400d3504225ec40d
Lonnie Souder II <lonnie2@souderfamily.net>
2021-02-17 14:47:44 -0500

    explicitly bind arg to reader over implicitly accessing obj from global scope

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import piconnections
 from piconnections import RaspberryPi
 
 from sensor_data import db, SensorDataLogger
+from functools import partial
 
 
 class GpioInterface:
@@ -23,6 +24,10 @@ class GpioInterface:
                             {'data': lights_on})
 
 
+def read_sensors(pi):
+    return (pi.temp_f, pi.humidity, pi.light_status, pi.moisture)
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
@@ -30,14 +35,12 @@ gpio_interface = GpioInterface(socketio)
 pi = RaspberryPi(gpio_interface)
 led_on = True
 
-def read_sensors():
-    return (pi.temp_f, pi.humidity, pi.light_status, pi.moisture)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sensor_data.db'
 db.init_app(app)
 with app.app_context():
     db.create_all()
-logger = SensorDataLogger(app, read_sensors)
+logger = SensorDataLogger(app, partial(read_sensors, pi))
+
 
 @app.route("/toggle_led", methods=['GET'])
 def toggle_led():
@@ -63,4 +66,5 @@ def handle_connect():
 if __name__ == '__main__':
     pi.start()
     logger.start()
-    socketio.run(app, host='0.0.0.0', port=8000, debug=True, use_reloader=False)
+    socketio.run(app, host='0.0.0.0', port=8000,
+                 debug=True, use_reloader=False)
